ee42b3cbe8b3d25d4d9121b4c5f9faa38edc55a5
John Freeman <john.freeman.opensource@gmail.com>
2016-08-29 19:50:35 +0100

    Added test for installing extensions (#31)
    Protect against regression defects.

---
+++ tests/test_install_extensions.py
@@ -0,0 +1,13 @@
+import pytest
+
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
+@pytest.mark.parametrize('extension', [
+    ('donjayamanne.python'),
+    ('wholroyd.jinja')
+])
+def test_visual_studio_code(Command, extension):
+    assert 'already installed' in Command.check_output('sudo --user test_usr -H code --install-extension ' + extension)
