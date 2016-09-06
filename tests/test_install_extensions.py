46b17011bfb43d2e4f824d7987b77981dfbab910
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 09:31:48 +0100

    Parameterized test syntax improvement (#37)
    Use simple array if there only a single param.

--- tests/test_install_extensions.py
+++ tests/test_install_extensions.py
@@ -6,8 +6,8 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 @pytest.mark.parametrize('extension', [
-    ('donjayamanne.python'),
-    ('wholroyd.jinja')
+    'donjayamanne.python',
+    'wholroyd.jinja'
 ])
 def test_visual_studio_code(Command, extension):
     output = Command.check_output('sudo --user test_usr -H code %s %s',
