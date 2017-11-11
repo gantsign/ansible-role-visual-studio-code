dd976b48f0b6866506a9878cf928c725f2e23f5e
John Freeman <john.freeman.opensource@gmail.com>
2017-11-11 11:46:58 +0000

    Fixed Python extension ID in test (#89)
    It would seem it's now an official Microsoft project.

--- tests/test_install_extensions.py
+++ tests/test_install_extensions.py
@@ -6,7 +6,7 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 @pytest.mark.parametrize('extension', [
-    'donjayamanne.python',
+    'ms-python.python',
     'wholroyd.jinja'
 ])
 def test_visual_studio_code(Command, extension):
