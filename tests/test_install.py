682090c943e85aa467c22695a2b0127dab5cb4f9
John Freeman <john.freeman.opensource@gmail.com>
2017-11-25 11:44:07 +0000

    Updated Molecule to 2.5
    To fix Travis CI build failure.

--- tests/test_install.py
+++
@@ -1,7 +0,0 @@
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
-
-
-def test_visual_studio_code(Command):
-    assert Command('which code').rc == 0
