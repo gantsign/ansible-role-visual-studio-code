0cb6e0538c4e56e771447fc3ec6b4a2df30b1437
John Freeman <john.freeman.opensource@gmail.com>
2018-04-10 17:17:13 +0000

    Added support for openSUSE 42.3
    Using `zypper` package manager.

---
+++ molecule/opensuse/tests/test_install_extensions.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('extension', [
+    'ms-python.python',
+    'wholroyd.jinja'
+])
+def test_visual_studio_code(Command, extension):
+    output = Command.check_output('sudo --user test_usr -H code %s %s',
+                                  '--install-extension', extension)
+    assert 'already installed' in output
