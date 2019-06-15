918c8fc7325d4d9c3bdc85e97989a9c84d21cd79
John Freeman <john.freeman.opensource@gmail.com>
2019-06-15 15:03:32 +0100

    Added support for VS Code Insiders builds (#151)
    For those who want to help test the latest features.

--- library/visual_studio_code_install_extension.py
+++ library/visual_studio_code_install_extension.py
@@ -10,8 +10,8 @@ from ansible.module_utils.basic import AnsibleModule
 __metaclass__ = type
 
 
-def is_extension_installed(module, name):
-    rc, out, err = module.run_command(['code', '--list-extensions', name])
+def is_extension_installed(module, executable, name):
+    rc, out, err = module.run_command([executable, '--list-extensions', name])
     if rc != 0 or err:
         module.fail_json(
             msg='Error querying installed extensions [%s]: %s' % (name,
@@ -21,9 +21,13 @@ def is_extension_installed(module, name):
     return match is not None
 
 
-def list_extension_dirs(module):
+def list_extension_dirs(module, executable):
+    dirname = '.vscode'
+    if executable == 'code-insiders':
+        dirname += '-insiders'
+
     ext_dir = os.path.expanduser(
-        os.path.join('~', '.vscode', 'extensions'))
+        os.path.join('~', dirname, 'extensions'))
 
     ext_dirs = [f for f in os.listdir(
         ext_dir) if os.path.isdir(os.path.join(ext_dir, f))]
@@ -31,25 +35,25 @@ def list_extension_dirs(module):
     return ext_dirs
 
 
-def install_extension(module, name):
-    if is_extension_installed(module, name):
+def install_extension(module, executable, name):
+    if is_extension_installed(module, executable, name):
         # Use the fact that extension directories names contain the version number
-        before_ext_dirs = list_extension_dirs(module)
+        before_ext_dirs = list_extension_dirs(module, executable)
         # Unfortunately `--force` suppresses errors (such as extension not found)
         rc, out, err = module.run_command(
-            ['code', '--install-extension', name, '--force'])
+            [executable, '--install-extension', name, '--force'])
         # Whitelist: [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues.
         if rc != 0 or (err and '[DEP0005]' not in err):
             module.fail_json(
                 msg='Error while upgrading extension [%s]: (%d) %s' % (name,
                                                                        rc,
                                                                        out + err))
-        after_ext_dirs = list_extension_dirs(module)
+        after_ext_dirs = list_extension_dirs(module, executable)
         changed = before_ext_dirs != after_ext_dirs
         return changed, 'upgrade'
     else:
         rc, out, err = module.run_command(
-            ['code', '--install-extension', name])
+            [executable, '--install-extension', name])
         # Whitelist: [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues.
         if rc != 0 or (err and '[DEP0005]' not in err):
             module.fail_json(
@@ -63,14 +67,19 @@ def install_extension(module, name):
 def run_module():
 
     module_args = dict(
+        executable=dict(type='str', required=False, choices=['code', 'code-insiders'], default='code'),
         name=dict(type='str', required=True))
 
     module = AnsibleModule(argument_spec=module_args,
                            supports_check_mode=False)
 
+    executable = module.params['executable']
+    if executable != 'code-insiders':
+        executable = 'code'
+
     name = module.params['name']
 
-    changed, change = install_extension(module, name)
+    changed, change = install_extension(module, executable, name)
 
     if changed:
         if change == 'upgrade':
--- molecule/code-only/tests/test_settings.py
+++ molecule/code-only/tests/test_settings.py
@@ -15,15 +15,3 @@ def test_settings(host):
     assert settings_file.group == 'test_usr'
     assert settings_file.mode == 0o600
     assert settings_file.contains('"Vagrantfile": "ruby"')
-
-
-def test_settings_insiders(host):
-    settings_file = host.file(
-        '/home/test_usr/.config/Code-Insiders/User/settings.json')
-
-    assert settings_file.exists
-    assert settings_file.is_file
-    assert settings_file.user == 'test_usr'
-    assert settings_file.group == 'test_usr'
-    assert settings_file.mode == 0o600
-    assert settings_file.contains('"Vagrantfile": "ruby"')
