ea4f2e8c3e708ce0dc1c6614645f4cdcb3f5c5e5
John Freeman <john.freeman.opensource@gmail.com>
2020-03-08 14:25:08 +0000

    Add test for visual_studio_code_settings_force_overwrite
    To ensure it works.

--- molecule/default/tests/test_settings.py
+++ molecule/default/tests/test_settings.py
@@ -27,3 +27,14 @@ def test_settings_insiders(host):
     assert settings_file.group == 'test_usr'
     assert settings_file.mode == 0o600
     assert settings_file.contains('"Vagrantfile": "ruby"')
+
+
+def test_settings_overwrite(host):
+    settings_file = host.file('/home/test_usr/.config/Code/User/settings.json')
+
+    assert settings_file.exists
+    assert settings_file.is_file
+    assert settings_file.user == 'test_usr'
+    assert settings_file.group == 'test_usr'
+    assert settings_file.mode == 0o600
+    assert not settings_file.contains('remove_me')
