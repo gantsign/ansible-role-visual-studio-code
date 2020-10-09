85a21c1c30f4ba46b4b0db2a3b48e3b9bcc618d7
John Freeman <john.freeman.opensource@gmail.com>
2020-10-09 17:03:45 +0100

    Updated Molecule to 3.0.8
    Keeping up with the latest changes.

--- molecule/default/tests/test_install.py
+++ molecule/default/tests/test_install.py
@@ -1,11 +1,3 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
 def test_visual_studio_code(host):
     assert host.run('which code').rc == 0
 
