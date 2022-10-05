11e92e4c1cde1568eaad3a38860391cec220ecea
John Freeman <john.freeman.opensource@gmail.com>
2022-10-05 16:09:56 +0100

    Added verification for keybindings tests
    To ensure files has been written.

--- molecule/default/tests/test_settings.py
+++ molecule/default/tests/test_settings.py
@@ -31,3 +31,41 @@ def test_settings_overwrite(host):
     assert settings_file.group == 'test_usr4'
     assert settings_file.mode == 0o600
     assert not settings_file.contains('remove_me')
+
+
+def test_keybindings(host):
+    settings_file = host.file(
+        '/home/test_usr/.config/Code/User/keybindings.json')
+
+    assert settings_file.exists
+    assert settings_file.is_file
+    assert settings_file.user == 'test_usr'
+    assert settings_file.group == 'test_usr'
+    assert settings_file.mode == 0o600
+    assert settings_file.contains(
+        '"command": "workbench.action.terminal.focus"')
+
+
+def test_keybindings_overwrite(host):
+    settings_file = host.file(
+        '/home/test_usr4/.config/Code/User/keybindings.json')
+
+    assert settings_file.exists
+    assert settings_file.is_file
+    assert settings_file.user == 'test_usr4'
+    assert settings_file.group == 'test_usr4'
+    assert settings_file.mode == 0o600
+    assert not settings_file.contains('remove_me')
+
+
+def test_keybindings_insiders(host):
+    settings_file = host.file(
+        '/home/test_usr/.config/Code - Insiders/User/keybindings.json')
+
+    assert settings_file.exists
+    assert settings_file.is_file
+    assert settings_file.user == 'test_usr'
+    assert settings_file.group == 'test_usr'
+    assert settings_file.mode == 0o600
+    assert settings_file.contains(
+        '"command": "workbench.action.terminal.focus"')
