ac9327fc643ee84df209be8ad7e6e799937a8ce5
John Freeman <john.freeman.opensource@gmail.com>
2022-10-05 22:32:22 +0100

    Fixed overwrite with empty settings (#223)
    Previously kept any existing file.

--- molecule/default/tests/test_settings.py
+++ molecule/default/tests/test_settings.py
@@ -22,11 +22,12 @@ def test_settings_insiders(host):
 
 
 def test_settings_overwrite(host):
-    settings_file = host.file('/home/test_usr/.config/Code/User/settings.json')
+    settings_file = host.file(
+        '/home/test_usr4/.config/Code/User/settings.json')
 
     assert settings_file.exists
     assert settings_file.is_file
-    assert settings_file.user == 'test_usr'
-    assert settings_file.group == 'test_usr'
+    assert settings_file.user == 'test_usr4'
+    assert settings_file.group == 'test_usr4'
     assert settings_file.mode == 0o600
     assert not settings_file.contains('remove_me')
